65475d66d8d8d5006936efa108332995e59164e2
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-07 06:36:41 +0700

    add automapper

---
+++ SourceCode/RealEstate/RealEstate.Api/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealEstate.Api.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.CreateMap<Post, PostViewModel>();
+            });
+        }
+    }
+}
