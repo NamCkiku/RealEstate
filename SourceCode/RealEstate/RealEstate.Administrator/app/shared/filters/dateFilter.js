1c0e6fa5af1dd709432b112c0eeaa8c9c19a34f3
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-11 00:42:56 +0700

    add angularjs

---
+++ SourceCode/RealEstate/RealEstate.Administrator/app/shared/filters/dateFilter.js
@@ -0,0 +1,19 @@
+﻿(function (app) {
+    app.filter("formatDate", ['$log', '$filter', '$rootScope', function ($log, $filter, $rootScope) {
+        return function (sDate) {
+            if (sDate != "" && sDate != undefined) {
+                return $filter('jsDate')(sDate, $filter('uppercase')($rootScope.RootScopeDateFormat));
+            } else {
+                return "";
+            }
+            return $filter('jsDate')(sDate, $filter('uppercase')($rootScope.RootScopeDateFormat));
+
+        };
+    }]);
+    app.filter("jsDate", function ($log) {
+        return function (x, formatDate) {
+            return moment(x).format(formatDate);
+
+        };
+    });
+})(angular.module('myApp'));
