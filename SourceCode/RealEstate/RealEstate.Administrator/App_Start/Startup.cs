15dbd518d6bbe93f4293963213b4d29cc9d0e6c8
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-10 20:56:34 +0700

    fix identity Administrator

---
+++ SourceCode/RealEstate/RealEstate.Administrator/App_Start/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+using RealEstate.Entities.Entites;
+using RealEstate.Repository.Infrastructure;
+using RealEstate.Repository.Repositories;
+using RealEstate.Service.Service;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using static RealEstate.Administrator.ApplicationUserStore;
+
+[assembly: OwinStartupAttribute(typeof(RealEstate.Administrator.Startup))]
+namespace RealEstate.Administrator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<RealEstateDbContext>().AsSelf().InstancePerRequest();
+
+
+            builder.RegisterType<RoleStore<AppRole>>().As<IRoleStore<AppRole, string>>();
+            //Asp.net Identity
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<AppUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationRoleManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(RoomTypeRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(RoomTypeService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+    }
+}
---
+++ SourceCode/RealEstate/RealEstate.Service/IService/IRoomTypeService.cs
@@ -0,0 +1,15 @@
+﻿using RealEstate.Entities.Entites;
+using RealEstate.Service.BaseService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Service.IService
+{
+    public interface IRoomTypeService : IService<RoomType>
+    {
+        IEnumerable<RoomType> GetAllRoomType();
+    }
+}
