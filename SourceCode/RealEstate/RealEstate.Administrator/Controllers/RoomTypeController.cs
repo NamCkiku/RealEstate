45654d0a0921a6999bf0a7bc392cca91c6f48c8d
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-11 15:28:13 +0700

    add roomtype administrator

---
+++ SourceCode/RealEstate/RealEstate.Administrator/Controllers/RoomTypeController.cs
@@ -0,0 +1,39 @@
+﻿using RealEstate.Entities.ModelView;
+using RealEstate.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Administrator.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        private readonly IRoomTypeService _roomTypeService;
+        public RoomTypeController(IRoomTypeService roomTypeService)
+        {
+            this._roomTypeService = roomTypeService;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult LoadAllRoomType(SearchEntity filter)
+        {
+            JsonResult jsonResult = new JsonResult();
+            try
+            {
+                HttpRequestBase request = this.HttpContext.Request;
+                var data = _roomTypeService.GetAll();
+                jsonResult = Json(new { success = true, lstData = data }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                throw ex;
+            }
+            return jsonResult;
+        }
+    }
+}
