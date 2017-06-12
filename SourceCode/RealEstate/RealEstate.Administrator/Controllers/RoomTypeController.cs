07d8bb48398f6aa7a96077ea9d4dde403766c5cc
Trần Hoàng Nam <namth@hn-binhanh.local>
2017-06-12 17:37:41 +0700

    add insert RoomType

--- SourceCode/RealEstate/RealEstate.Administrator/Controllers/RoomTypeController.cs
+++ SourceCode/RealEstate/RealEstate.Administrator/Controllers/RoomTypeController.cs
@@ -1,4 +1,7 @@
-﻿using RealEstate.Entities.ModelView;
+﻿using RealEstate.Administrator.Infrastructure.Extensions;
+using RealEstate.Administrator.Models.ViewModel;
+using RealEstate.Entities.Entites;
+using RealEstate.Entities.ModelView;
 using RealEstate.Service.IService;
 using System;
 using System.Collections.Generic;
@@ -19,14 +22,75 @@ namespace RealEstate.Administrator.Controllers
         {
             return View();
         }
-        public JsonResult LoadAllRoomType(SearchEntity filter)
+        public JsonResult LoadAllRoomType(SearchRoomTypeEntity filter)
         {
             JsonResult jsonResult = new JsonResult();
             try
             {
                 HttpRequestBase request = this.HttpContext.Request;
-                var data = _roomTypeService.GetAll();
+                var data = _roomTypeService.GetAllRoomType(filter);
                 jsonResult = Json(new { success = true, lstData = data }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                throw ex;
+            }
+            return jsonResult;
+        }
+        public JsonResult InsertRoomType(RoomTypeViewModel roomtypevm)
+        {
+            JsonResult jsonResult = new JsonResult();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var roomType = new RoomType();
+                    roomType.UpdateRoomType(roomtypevm);
+                    roomType.CreatedDate = DateTime.Now;
+                    roomType.CreatedBy = User.Identity.Name;
+                    var result = _roomTypeService.Insert(roomType);
+                    _roomTypeService.SaveChanges();
+                    if (result != null)
+                    {
+                        jsonResult = Json(new { success = true, objData = result }, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                else
+                {
+                    jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                throw ex;
+            }
+            return jsonResult;
+        }
+        public JsonResult UpdateRoomType(RoomTypeViewModel roomtypevm)
+        {
+            JsonResult jsonResult = new JsonResult();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var roomType = _roomTypeService.GetById(roomtypevm.ID);
+                    roomType.UpdateRoomType(roomtypevm);
+                    roomType.UpdatedDate = DateTime.Now;
+                    roomType.UpdatedBy = User.Identity.Name;
+                    _roomTypeService.Update(roomType);
+                    _roomTypeService.SaveChanges();
+                    jsonResult = Json(new { success = true }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                }
             }
             catch (Exception ex)
             {
--- SourceCode/RealEstate/RealEstate.Service/Service/RoomTypeService.cs
+++ SourceCode/RealEstate/RealEstate.Service/Service/RoomTypeService.cs
@@ -1,4 +1,5 @@
 ﻿using RealEstate.Entities.Entites;
+using RealEstate.Entities.ModelView;
 using RealEstate.Repository.Infrastructure;
 using RealEstate.Service.BaseService;
 using RealEstate.Service.IService;
@@ -16,9 +17,28 @@ namespace RealEstate.Service.Service
         {
         }
 
-        public IEnumerable<RoomType> GetAllRoomType()
+        public IEnumerable<RoomType> GetAllRoomType(SearchRoomTypeEntity filter)
         {
-            return null;
+            List<RoomType> lstroomType = new List<RoomType>();
+            DateTime st = filter.StartDate == null ? DateTime.MinValue : filter.StartDate.Value.Date;
+            DateTime et = filter.EndDate == null ? DateTime.MaxValue : filter.EndDate.Value.Date.AddDays(1);
+            if (filter.Keywords == null)
+            {
+                filter.Keywords = "";
+            }
+            try
+            {
+                lstroomType = _repository.GetMulti(x => x.Status == filter.Status
+                && (filter.StartDate == null || x.CreatedDate >= st || x.CreatedDate == null)
+                && (filter.EndDate == null || x.CreatedDate < et || x.CreatedDate == null)
+                && (x.RoomTypeName.Contains(filter.Keywords) || x.Description.Contains(filter.Keywords) || string.IsNullOrEmpty(filter.Keywords))
+                ).OrderByDescending(x => x.CreatedDate).ToList();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return lstroomType;
         }
     }
 }
