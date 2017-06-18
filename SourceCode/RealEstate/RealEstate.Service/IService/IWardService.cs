ee292c52c969762790946887f1d948efea645f32
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-18 10:12:42 +0700

    add api roomtype and ward,district

---
+++ SourceCode/RealEstate/RealEstate.Service/IService/IWardService.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate.Entities.Entites;
+using RealEstate.Service.BaseService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Service.IService
+{
+    public interface IWardService : IService<Ward>
+    {
+    }
+}
