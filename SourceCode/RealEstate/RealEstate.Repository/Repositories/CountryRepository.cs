d7aa1cf4bf02d62ba85a1d2db831d98f8e5940b6
NamCkiku <tranhoangnam11373@gmail.com>
2017-06-27 20:56:24 +0700

    add dapper

--- SourceCode/RealEstate/RealEstate.Repository/Repositories/CountryRepository.cs
+++ SourceCode/RealEstate/RealEstate.Repository/Repositories/CountryRepository.cs
@@ -1,9 +1,13 @@
-﻿using RealEstate.Common.Extensions.Data;
+﻿using Dapper;
+using RealEstate.Common.Extensions.Data;
 using RealEstate.Entities.Entites;
 using RealEstate.Repository.Infrastructure;
 using RealEstate.Repository.IRepositories;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +33,14 @@ namespace RealEstate.Repository.Repositories
             {
                 //log.FatalFormat("{0} has an exception:{1}", MethodInfo.GetCurrentMethod().Name, ex);
                 return new List<Country>();
+            }
+        }
+
+        public IEnumerable<Country> GetAllCountryDapper()
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["RealEstateConnection"].ConnectionString))
+            {
+                return db.Query<Country>("SELECT * FROM COUNTRY");
             }
         }
     }
--- SourceCode/RealEstate/RealEstate.Repository/Repositories/RoomRepository.cs
+++ SourceCode/RealEstate/RealEstate.Repository/Repositories/RoomRepository.cs
@@ -6,13 +6,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RealEstate.Entities.ModelView;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
 
 namespace RealEstate.Repository.Repositories
 {
     public class RoomRepository : RepositoryBase<Room>, IRoomRepository
     {
+        private IDbConnection _db = new SqlConnection(ConfigurationManager.ConnectionStrings["RealEstateConnection"].ConnectionString);
         public RoomRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+        }
+
+        public IEnumerable<RoomEntity> GetAllRoom()
+        {
+            return _db.Query<RoomEntity>("SELECT * FROM ROOM");
         }
     }
 }
