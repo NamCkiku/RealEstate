86a10ec485e0d62320ec990c7c8c6051489b6283
NamCkiku <tranhoangnam11373@gmail.com>
2017-07-04 21:49:48 +0700

    add service

---
+++ SourceCode/RealEstateClient/RealEstateWeb/RealEstate-Web/src/app/core/services/authen.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { SystemConstants } from '../../core/common/system.constants';
+import { LoggedInUser } from '../domain/loggedin.user';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class AuthenService {
+
+  constructor(private _http: Http) { }
+
+  login(username: string, password: string) {
+    let body = "userName=" + encodeURIComponent(username) +
+      "&password=" + encodeURIComponent(password) +
+      "&grant_type=password";
+    let headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(SystemConstants.BASE_API + '/api/oauth/token', body, options).map((response: Response) => {
+      let user: LoggedInUser = response.json();
+      if (user && user.access_token) {
+        localStorage.removeItem(SystemConstants.CURRENT_USER);
+        localStorage.setItem(SystemConstants.CURRENT_USER, JSON.stringify(user));
+      }
+    });
+  }
+  logout() {
+    localStorage.removeItem(SystemConstants.CURRENT_USER);
+  }
+
+  isUserAuthenticated(): boolean {
+    let user = localStorage.getItem(SystemConstants.CURRENT_USER);
+    if (user != null) {
+      return true;
+    }
+    else
+      return false;
+  }
+
+  getLoggedInUser(): LoggedInUser {
+    let user: LoggedInUser;
+    if (this.isUserAuthenticated()) {
+      var userData = JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
+      user = new LoggedInUser(userData.access_token,
+        userData.username,
+        userData.fullName,
+        userData.email,
+        userData.avatar, userData.roles, userData.permissions);
+    }
+    else
+      user = null;
+    return user;
+  }
+  checkAccess(functionId: string) {
+    var user = this.getLoggedInUser();
+    var result: boolean = false;
+    var permission: any[] = JSON.parse(user.permissions);
+    var roles: any[] = JSON.parse(user.roles);
+    var hasPermission: number = permission.findIndex(x => x.FunctionId == functionId && x.CanRead == true);
+    if (hasPermission != -1 || roles.findIndex(x => x == "Admin") != -1) {
+      return true;
+    }
+    else
+      return false;
+  }
+  hasPermission(functionId: string, action: string): boolean {
+    var user = this.getLoggedInUser();
+    var result: boolean = false;
+    var permission: any[] = JSON.parse(user.permissions);
+    var roles: any[] = JSON.parse(user.roles);
+    switch (action) {
+      case 'create':
+        var hasPermission: number = permission.findIndex(x => x.FunctionId == functionId && x.CanCreate == true);
+        if (hasPermission != -1 || roles.findIndex(x => x == "Admin") != -1)
+          result = true;
+        break;
+      case 'update':
+        var hasPermission: number = permission.findIndex(x => x.FunctionId == functionId && x.CanUpdate == true);
+        if (hasPermission != -1 || roles.findIndex(x => x == "Admin") != -1)
+          result = true;
+        break;
+      case 'delete':
+        var hasPermission: number = permission.findIndex(x => x.FunctionId == functionId && x.CanDelete == true);
+        if (hasPermission != -1 || roles.findIndex(x => x == "Admin") != -1)
+          result = true;
+        break;
+    }
+    return result;
+  }
+}
---
+++ SourceCode/RealEstateClient/RealEstateWeb/RealEstate-Web/src/app/core/services/data.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Router } from '@angular/router';
+import { SystemConstants } from './../common/system.constants';
+import { AuthenService } from './authen.service';
+import { NotificationService } from './notification.service';
+import { UtilityService } from './utility.service';
+
+import { Observable } from 'rxjs/Observable';
+import { MessageContstants } from './../common/message.constants';
+
+@Injectable()
+export class DataService {
+  private headers: Headers;
+  constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,
+    private _notificationService: NotificationService, private _utilityService: UtilityService) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+  }
+
+  get(uri: string) {
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).map(this.extractData);
+  }
+  post(uri: string, data?: any) {
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
+  }
+  put(uri: string, data?: any) {
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
+  }
+  delete(uri: string, key: string, id: string) {
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers })
+      .map(this.extractData);
+  }
+  deleteWithMultiParams(uri: string, params) {
+    this.headers.delete('Authorization');
+
+    this.headers.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    var paramStr: string = '';
+    for (let param in params) {
+      paramStr += param + "=" + params[param] + '&';
+    }
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + paramStr, { headers: this.headers })
+      .map(this.extractData);
+
+  }
+  postFile(uri: string, data?: any) {
+    let newHeader = new Headers();
+    newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader })
+      .map(this.extractData);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+  public handleError(error: any) {
+    if (error.status == 401) {
+      localStorage.removeItem(SystemConstants.CURRENT_USER);
+      this._notificationService.printErrorMessage(MessageContstants.LOGIN_AGAIN_MSG);
+      this._utilityService.navigateToLogin();
+    }
+    else if (error.status == 403) {
+      localStorage.removeItem(SystemConstants.CURRENT_USER);
+      this._notificationService.printErrorMessage(MessageContstants.FORBIDDEN);
+      this._utilityService.navigateToLogin();
+    }
+    else {
+      let errMsg = JSON.parse(error._body).Message;
+      this._notificationService.printErrorMessage(errMsg);
+
+      return Observable.throw(errMsg);
+    }
+  }
+}
