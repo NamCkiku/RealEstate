b54aec6eb99f7c1ac23932f67fd1e1e179c6bb49
namth <namth@PPM-NAMTH-PC>
2017-07-15 17:01:18 +0700

    fix error servicefactory

---
+++ SourceCode/RealEstate/RealEstate.Common/Enumerations/AuditLogType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Common.Enumerations
+{
+    public enum AuditLogType
+    {
+        Create = 1,
+        Login = 2
+    }
+}
