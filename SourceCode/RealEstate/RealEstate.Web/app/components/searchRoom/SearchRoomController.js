c16595d176c639e02b807085ec4c5ac0c5bf242a
namth <namth@PPM-NAMTH-PC>
2017-08-22 10:29:31 +0700

    update broadcast on in search rooom

--- SourceCode/RealEstate/RealEstate.Web/app/components/searchRoom/SearchRoomController.js
+++ SourceCode/RealEstate/RealEstate.Web/app/components/searchRoom/SearchRoomController.js
@@ -19,13 +19,10 @@
             lstProvince: [],
             lstDistrict: [],
             lstWard: [],
-            listRoom : []
         }
         $scope.init = function () {
-
             $scope.GetAllRoomType();
             $scope.GetAllProvince();
-            $scope.FilterRoom();
         };
         $scope.sliderFrice = {
             minValue: 0,
@@ -135,66 +132,14 @@
             $window.location.href = '/RoomList/Index?' + queryString.join('&');
         }
 
-        // Lọc phòng theo tiêu chí
-        $scope.FilterRoom = function () {
-            var myBlockUI = blockUI.instances.get('BlockUIRoom');
-            myBlockUI.start();
-            alert($scope.sortKey);
-            var config = {
-                params: {
-                    RoomTypeID: $scope.fillter.roomtype,
-                    PriceFrom: $scope.sliderFrice.minValue,
-                    PriceTo: $scope.sliderFrice.maxValue,
-                    WardID: $scope.fillter.wardID,
-                    DistrictID: $scope.fillter.districtID,
-                    ProvinceID: $scope.fillter.provinceId,
-                    Keywords: null,
-                    StartDate: null,
-                    EndDate: null,
-                    Status: true,
-                    page: 1,
-                    pageSize: 10,
-                    sort: "ASC"
-                }
-            }
-            apiService.get('api/room/getallroomfullsearch', config, function (respone) {
-                $scope.data.listRoom = respone.data.items;
-                console.log($scope.data.listRoom);
-                myBlockUI.stop();
-            }, function (respone) {
-                myBlockUI.stop();
-                BaseService.displayError("Không lấy được dữ liệu xã phường", 3000);
-            });
-        }
+        $scope.fireLoadFilterEvent = function () {
+            $scope.$broadcast('fireLoadFilterEvent', $scope.fillter);
+        };
 
         $scope.showFilter = function () {
             $('#open-filters').toggleClass('openf');
             $('.dqdt-sidebar').toggleClass('openf');
         }
-
-        var QueryString = function () {
-            // This function is anonymous, is executed immediately and 
-            // the return value is assigned to QueryString!
-            var query_string = {};
-            var query = window.location.search.substring(1);
-            var vars = query.split("&");
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split("=");
-                // If first entry with this name
-                if (typeof query_string[pair[0]] === "undefined") {
-                    query_string[pair[0]] = decodeURIComponent(pair[1]);
-                    // If second entry with this name
-                } else if (typeof query_string[pair[0]] === "string") {
-                    var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
-                    query_string[pair[0]] = arr;
-                    // If third or later entry with this name
-                } else {
-                    query_string[pair[0]].push(decodeURIComponent(pair[1]));
-                }
-            }
-            return query_string;
-        }();
-
         $scope.init();
     }
 })(angular.module('myApp'));
