32443919ddb7f58f86a3aeb93f99e576c8b01d37
NamCkiku <tranhoangnam11373@gmail.com>
2017-10-08 11:16:07 +0700

    add payment ngan luong

---
+++ SourceCode/RealEstate/RealEstate.Api/Controllers/PaymentController.cs
@@ -0,0 +1,149 @@
+﻿using RealEstate.Api.Models.ViewModel;
+using RealEstate.Api.NganLuongAPI;
+using RealEstate.Common.Helper;
+using RealEstate.Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RealEstate.Api.Controllers
+{
+    /// <summary>
+    /// Class dùng để thanh toán tiền,....
+    /// </summary>
+    /// <Modified>
+    /// Name     Date         Comments
+    /// namth  6/7/2017   created
+    /// </Modified>
+    /// <seealso cref="RealEstate.Api.Controllers.ApiControllerBase" />
+    [RoutePrefix("api/payment")]
+    // [Authorize]
+    public class PaymentController : ApiControllerBase
+    {
+        private string merchantId = ConfigHelper.GetByKey("MerchantId");
+        private string merchantPassword = ConfigHelper.GetByKey("MerchantPassword");
+        private string merchantEmail = ConfigHelper.GetByKey("MerchantEmail");
+
+        public PaymentController(IErrorService errorService) : base(errorService)
+        {
+
+        }
+        /// <summary>
+        /// Hàm API thanh toán bằng ngân hàng
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>Link API:api/payment/paymentbank</returns>
+        /// <Modified>
+        /// Name     Date         Comments
+        /// namth  17/09/2017   created
+        /// </Modified>
+        [Route("paymentbank")]
+        [HttpPost]
+        public HttpResponseMessage PaymentBank(HttpRequestMessage request, PaymentViewModel paymentviewmodel)
+        {
+            HttpResponseMessage responeResult = new HttpResponseMessage();
+            try
+            {
+                responeResult = CreateHttpResponse(request, () =>
+                {
+                    HttpResponseMessage response = null;
+
+                    RequestCheckoutInfo info = new RequestCheckoutInfo();
+                    info.Merchant_id = merchantId;
+                    info.Merchant_password = merchantPassword;
+                    info.Receiver_email = merchantEmail;
+
+
+
+                    info.cur_code = "vnd";
+                    info.bank_code = paymentviewmodel.BankCode;
+
+                    info.Order_code = paymentviewmodel.OrderCode.ToString();
+                    info.Total_amount = paymentviewmodel.TotalAmount.ToString();
+                    info.fee_shipping = "0";
+                    info.Discount_amount = "0";
+                    info.order_description = "Nạp tiền vào tài khoản tại Bizland.vn";
+                    info.return_url = "";
+                    info.cancel_url = "";
+
+                    info.Buyer_fullname = paymentviewmodel.UserName;
+                    info.Buyer_email = paymentviewmodel.Email;
+                    info.Buyer_mobile = paymentviewmodel.PhoneNumber.ToString();
+
+                    APICheckoutV3 objNLChecout = new APICheckoutV3();
+                    ResponseInfo result = objNLChecout.GetUrlCheckout(info, paymentviewmodel.PaymentMethod);
+                    if (result.Error_code == "00")
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, result);
+                    }
+                    else
+                    {
+                        response = request.CreateResponse(HttpStatusCode.BadRequest, result.Description);
+                    }
+
+                    return response;
+                });
+            }
+            catch (Exception ex)
+            {
+                Common.Logs.LogCommon.WriteLogError(ex.Message);
+            }
+            return responeResult;
+        }
+
+        /// <summary>
+        /// Hàm API thanh toán thẻ điện thoại
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>Link API:api/payment/paymentcash</returns>
+        /// <Modified>
+        /// Name     Date         Comments
+        /// namth  17/09/2017   created
+        /// </Modified>
+        [Route("paymentcash")]
+        [HttpPost]
+        public HttpResponseMessage PaymentCash(HttpRequestMessage request, PaymentViewModel paymentviewmodel)
+        {
+            HttpResponseMessage responeResult = new HttpResponseMessage();
+            try
+            {
+                responeResult = CreateHttpResponse(request, () =>
+                {
+                    HttpResponseMessage response = null;
+
+                    RequestInfo info = new RequestInfo();
+                    info.Merchant_id = merchantId;
+                    //Email tài khoản nhận tiền khi nạp
+                    info.Merchant_acount = merchantEmail;
+                    info.Merchant_password = merchantPassword;
+
+                    //Nhà mạng
+                    info.CardType = paymentviewmodel.CardType;
+                    info.Pincard = paymentviewmodel.Pincard;
+
+                    info.Refcode = (new Random().Next(0, 10000)).ToString();
+                    info.SerialCard = paymentviewmodel.SerialCard;
+
+                    ResponseCashInfo resutl = NLCardLib.CardChage(info);
+                    if (resutl.Errorcode.Equals("00"))
+                    {
+                        response = request.CreateResponse(HttpStatusCode.OK, resutl);
+                    }
+                    else
+                    {
+                        response = request.CreateResponse(HttpStatusCode.BadRequest, resutl);
+                    }
+                    return response;
+                });
+            }
+            catch (Exception ex)
+            {
+                Common.Logs.LogCommon.WriteLogError(ex.Message);
+            }
+            return responeResult;
+        }
+    }
+}
---
+++ SourceCode/RealEstate/RealEstate.Api/Models/ViewModel/PaymentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealEstate.Api.Models.ViewModel
+{
+    public class PaymentViewModel
+    {
+        public string PaymentMethod { get; set; }
+        public string BankCode { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public int OrderCode { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string CardType { get; set; }
+
+
+        public string Pincard { get; set; }
+
+        public string SerialCard { get; set; }
+    }
+}
